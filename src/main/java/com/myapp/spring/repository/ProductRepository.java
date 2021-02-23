cd293de2f2b3356d2ca080d24277717361173bc9
VennelaB <vennelabandla1@gmail.com>
2021-02-23 14:45:35 +0530

    sonar integrtion

--- src/main/java/com/myapp/spring/repository/ProductRepository.java
+++ src/main/java/com/myapp/spring/repository/ProductRepository.java
@@ -1,19 +1,11 @@
 package com.myapp.spring.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
+
 import com.myapp.spring.model.Product;
 
-import java.util.List;
-
-public interface ProductRepository {
-	
-	Product saveProduct(Product product);
-	
-	Product upadateProduct(Product product);
-	
-	List<Product> findAll();
-	
-	void delete(Integer productId);
-	
-	Product findById(Integer id);
+@RestResource(path = "products")
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 
 }
